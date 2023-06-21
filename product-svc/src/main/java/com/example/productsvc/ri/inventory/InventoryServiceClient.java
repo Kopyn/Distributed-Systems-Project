ab7d76477c4dedd209da5e6344e4e557056f0fa4
mkopinski <mkopinski@griddynamics.com>
2023-06-21 12:58:28 +0200

    Gateway and registry services

--- product-svc/src/main/java/com/example/productsvc/ri/inventory/InventoryServiceClient.java
+++ product-svc/src/main/java/com/example/productsvc/ri/inventory/InventoryServiceClient.java
@@ -1,5 +1,12 @@
 package com.example.productsvc.ri.inventory;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(value = "inventory-svc", url = "http://localhost:8082/")
 public interface InventoryServiceClient {
-    boolean isProductAvailable(String uniqId);
+    @RequestMapping(method = RequestMethod.GET, value = "?uniqId={uniqId}")
+    boolean isProductAvailable(@PathVariable("uniqId") String uniqId);
 }
