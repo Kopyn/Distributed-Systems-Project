9ab674eff257d2a04f6b8c7e53aed7c45f311d3c
mkopinski <mkopinski@griddynamics.com>
2023-06-21 12:58:28 +0200

    Gateway and registry services

--- product-svc/src/main/java/com/example/productsvc/ri/catalog/CatalogServiceClient.java
+++ product-svc/src/main/java/com/example/productsvc/ri/catalog/CatalogServiceClient.java
@@ -1,10 +1,17 @@
 package com.example.productsvc.ri.catalog;
 
 import com.example.productsvc.model.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
+@FeignClient(value = "catalog-svc", url = "http://localhost:8083/Product/")
 public interface CatalogServiceClient {
-    Product getProductDataById(String uniqId);
-    List<Product> getProductDataBySku(String sku);
+    @RequestMapping(method = RequestMethod.GET, value = "GetProductByUniqId?uniqId={uniqId}")
+    Product getProductDataById(@PathVariable("uniqId") String uniqId);
+    @RequestMapping(method = RequestMethod.GET, value = "GetProductsBySku?sku={sku}")
+    List<Product> getProductDataBySku(@PathVariable("sku") String sku);
 }
