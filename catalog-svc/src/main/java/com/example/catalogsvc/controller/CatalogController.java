6e9d2014a0e85bce626702367d829b9fee8d7ce1
mkopinski <mkopinski@griddynamics.com>
2023-06-23 13:20:19 +0200

    Code refactor

--- catalog-svc/src/main/java/com/example/catalogsvc/controller/CatalogController.java
+++ catalog-svc/src/main/java/com/example/catalogsvc/controller/CatalogController.java
@@ -2,12 +2,10 @@ package com.example.catalogsvc.controller;
 
 import com.example.catalogsvc.manager.ProductManager;
 import com.example.catalogsvc.model.Product;
-import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
